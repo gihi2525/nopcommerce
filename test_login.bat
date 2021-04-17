5f17a36e4036bc03a0b2853598acf3b2ff69ae4c
gihi2525 <igal.zabo@gmail.com>
2021-04-17 19:56:51 +0300

    first commit

--- test_login.bat
+++ test_login.bat
@@ -1,2 +1,2 @@
-rem pytest -s -v --html=./Reports/q.html testCases/test_login.py --browser firefox
-pytest -s -v -m="functionality" --html=./Reports/r.html testCases\test_login.py
+pytest -s -v  --browser firefox
+rem pytest -s -v -m="functionality" --html=./Reports/r.html testCases\test_login.py
